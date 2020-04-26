8ad059368d3d901e0541154267921ed69c37a76a
Dylan Bolter <dylanbolter@Dylans-MacBook-Pro.local>
2020-04-26 16:39:48 -0500

    removed the contact me page + changed txt

--- site.py
+++ site.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/contact")
-def contact():
-    return render_template('contact.html', title='Contact')
-
 @app.route("/work")
 def work():
     return render_template('work.html', title='Work')
